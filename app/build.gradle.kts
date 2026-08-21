63e1c93acbd377306d8e5d36187501b7f288c432
chrisgia <giangreco.christian@gmail.com>
2026-08-22 00:22:31 +0200

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "io.github.cgiangreco.restoreroundedsliders"
         minSdk = 35
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0"
+        versionCode = 7
+        versionName = "1.1"
     }
 }
 
